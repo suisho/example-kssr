d7ec7d1bc0a592496f7bf8cae74a71308f2e9be6
suisho <suisho000000@gmail.com>
2014-05-02 01:04:01 +0900

    add kss

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Kssr::Application.routes.draw do
+  mount Kss::Engine => '/kss' if Rails.env.development?
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
