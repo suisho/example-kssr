50b43083cad1da2148b7a9832ab41a6e73fc2463
suisho <suisho000000@gmail.com>
2014-05-08 00:32:53 +0900

    simple controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Kssr::Application.routes.draw do
-  mount Kss::Engine => '/kss' if Rails.env.development?
+  mount Kss::Engine => '/styleguide' if Rails.env.development?
+  get '/styleguide/(:section)' => 'styleguide#show'
+  #get '/styleguide/button' => 'styleguide#button'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
